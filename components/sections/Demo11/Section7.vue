5d0cd8a47d281508af17080964e7cabca1edd4a1
João Firmino <firminoata@gmail.com>
2023-10-18 21:40:11 -0300

    feat: TMP animations

--- components/sections/Demo11/Section7.vue
+++ components/sections/Demo11/Section7.vue
@@ -1,54 +1,103 @@
 <template>
     <section id="tmprgm" class="pt-100 pb-150 border-top-blue ct-08 content-section division">
-        
-        <div class="pagTitle wow animated" data-animation="fadeInUp">
-            TMP
-        </div>
-        <div class="pagCircle wow animated" data-animation="fadeInUp">
-            1
-        </div>
-
         <div class="container">
             <div class="row">
-                <div class="col-12 wow animated" data-animation="fadeInUp">
-                    <h2 class="s-24 w-700 text-center mb-5">
-                        {{ $t('section6_col1_subtitle') }} 
-                    </h2>
-
-                    <div class="fbox-12 bg--black-300 r-28 nav-cards wow animated" data-animation="fadeInLeft">
-
-                        <div class="exbox ">
-
-                            <h4 class="s-16 w-700 text-white mb-2">
-                                {{ $t('section6_col1_pos1_title') }}:
-                            </h4>
-
-                            <p class="s-14 text-white">
-                                {{ $t('section6_col1_pos1_text') }} <br><br><br>
-                            </p>
+                <div class="col-12 fbox-12 bg-tpm pt-70 pl-50 pr-50 fbox-shadow bg--black-300 r-28 wow animated" data-animation="fadeIn">
+                    <div class="row mb-50 wow animated" data-animation="fadeInLeft">
+                        <div class="col-4 pr-30 b-right-2 text-right">
+                            <h2 class="s-38 w-700 pt-20 text-white">
+                                <span class="numbering">1</span> TMP
+                            </h2>
                         </div>
 
-                        <div class="exbox">
-                            <h4 class="s-16 w-700 text-white mb-2">
-                                {{ $t('section6_col1_pos2_title') }}:
-                            </h4>
-
-                            <p class="s-14 text-white">
-                                {{ $t('section6_col1_pos2_text') }}<br>
-                            </p>
-                        </div>
-
-                        <div class="exbox">
-                            <h4 class="s-16 w-700 text-white mb-2">
-                                {{ $t('section6_col1_pos3_title') }}:
-                            </h4>
-
-                            <p class="s-14 text-white">
-                                {{ $t('section6_col1_pos3_text') }}
-                            </p>
+                        <div class="col-7 pl-30">
+                            <h2 class="s-18 w-700 text-white">
+                                {{ $t('section6_col1_subtitle') }}: 
+                            </h2>
                         </div>
                     </div>
-                    
+
+                    <div class="row nav-cards mb-80 wow animated" data-animation="fadeInRight">
+                        <div class="col-4 d-flex">
+                            <div class="exbox">
+                                <h4 class="s-14 w-700 text-white mb-2">
+                                    {{ $t('section6_col1_pos1_title') }}:
+                                </h4>
+
+                                <p class="s-13 text-white">
+                                    {{ $t('section6_col1_pos1_text') }}
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="col-4 d-flex">
+                            <div class="exbox">
+                                <h4 class="s-14 w-700 text-white mb-2">
+                                    {{ $t('section6_col1_pos2_title') }}:
+                                </h4>
+
+                                <p class="s-13 text-white">
+                                    {{ $t('section6_col1_pos2_text') }}
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="col-4 d-flex">
+                            <div class="exbox">
+                                <h4 class="s-14 w-700 text-white mb-2">
+                                    {{ $t('section6_col1_pos3_title') }}:
+                                </h4>
+
+                                <p class="s-13 text-white">
+                                    {{ $t('section6_col1_pos3_text') }}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row justify-content-md-end mb-50 wow animated" data-animation="fadeInRight">
+                        <div class="col-7 pr-30 text-right b-right-2">
+                            <h2 class="s-18 w-700 text-white">
+                                {{ $t('section6_col2_subtitle') }} 
+                            </h2>
+                        </div>
+
+                        <div class="col-4 pl-30">
+                            <h2 class="s-38 w-700 pt-30 text-white">
+                                RGM <span class="numbering ml-20">2</span>
+                            </h2>
+                        </div>
+                    </div>
+
+                    <div class="row justify-content-md-center nav-cards mb-50 wow animated" data-animation="fadeInLeft">
+                        <div class="col-4 d-flex">
+                            <div class="exbox">
+                                <h4 class="s-14 w-700 text-white mb-2">
+                                    {{ $t('section6_col2_pos2_title') }}:
+                                </h4>
+
+                                <p class="s-13 text-white">
+                                    {{ $t('section6_col2_pos2_text') }}
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="col-4 d-flex">
+                            <div class="exbox">
+                                <h4 class="s-14 w-700 text-white mb-2">
+                                    {{ $t('section6_col2_pos1_title') }}:
+                                </h4>
+
+                                <p class="s-13 text-white">
+                                    {{ $t('section6_col2_pos1_text') }}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="cell-hand wow animated" data-animation="fadeInLeft">
+                        <img class="img-fluid image" src="/assets/images/mao-celular.png" alt="Cell Hand">
+                    </div>
                 </div>
             </div>
         </div>
